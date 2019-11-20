55615f0acecce98cf81ad5cc8347415311ad7fb6
Sid <navyasiddhu@gmail.com>
2019-11-20 11:03:17 +0800

    test

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>CTC Global</h1>
 <?php
 echo "<body style='background-color:blue'>";
-echo "Hello World!";
 echo "<div align='center'>Hello CTC</div>"; 
 ?>
 
