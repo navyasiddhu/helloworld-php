d197bdb62a19a271d5ee1d755290c38a66889a0e
Sid <navyasiddhu@gmail.com>
2019-11-20 11:00:06 +0800

    test

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 
 <h1>My first PHP page</h1>
 <?php
-echo "<body style='background-color:red'>";
+echo "<body style='background-color:blue'>";
 echo "Hello World!";
+echo "<div align='center'>Hello CTC</div>"; 
 ?>
 
 </body>
