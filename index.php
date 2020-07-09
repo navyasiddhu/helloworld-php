17419c286a2a025cf0cf860099e024cd265e434f
navyasiddhu <navyasiddhu@gmail.com>
2020-07-09 08:00:49 +0800

    added hostname

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 <?php
 echo "<body style='background-color:blue'>";
 echo '<h1 align="center">Welcome CTC</h1>';
+
 echo "!!!!! Howdy folks !!!!!!!"; 
+$hostname = file_get_contents('/etc/hostname', true); 
+echo "My name is $hostname" 
+ 
 ?>
 
 </body>
