ef33f5baf5999ddad25a450500e86bcbb0d95ce1
navyasiddhu <navyasiddhu@gmail.com>
2020-07-09 08:14:16 +0800

    adding container name

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
- <!DOCTYPE html>
-<html>
-<body>
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"
 
+</head>
+
+<body style='background-color:blue'>
 <title>CTC Global</title>
-<?php
-echo "<body style='background-color:blue'>";
-echo '<h1 align="center">Welcome CTC</h1>';
+<h1>Welcome to the DevOps Demo Application v0.</h1>
+<p><i>This app is used for demonstrating and testing various sample deployments</i></p>
+<h3>Connection to MySQL DB:
+		<?php
 
-echo "!!!!! Howdy folks !!!!!!!"; 
-$hostname = file_get_contents('/etc/hostname', true); 
-echo "My name is $hostname" 
- 
-?>
+
+		echo "!!!!! Howdy folks !!!!!!!"; 
+		$hostname = file_get_contents('/etc/hostname', true); 
+		echo "My name is $hostname" 
+		 
+		?>
+						
+                   
+</h3>
 
 </body>
 </html> 
